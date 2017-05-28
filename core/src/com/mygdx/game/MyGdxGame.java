e2bdfc3f16e384dcab335daa6c152f5282e4eda1
Ancifa <ancifa@yahoo.com>
2017-05-28 18:30:46 +0300

    creation of Space and Background classes

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -8,26 +8,34 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	
+	Background background;
+	SpaceShip spaceShip;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		background = new Background();
+		spaceShip = new SpaceShip();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		update();
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		background.render(batch);
+		spaceShip.render(batch);
 		batch.end();
 	}
-	
+
+	public void update() {
+		background.update();
+		spaceShip.update();
+	}
+
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
---
+++ core/src/com/mygdx/game/SpaceShip.java
@@ -0,0 +1,44 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * Created by 1 on 28.05.2017.
+ */
+public class SpaceShip {
+    private Texture texture;
+    private Vector2 position;
+    private float speed;
+
+    private int screenHeight = Gdx.graphics.getHeight();
+    private int screenWidth = Gdx.graphics.getWidth();
+
+    public SpaceShip() {
+        texture = new Texture("spaceship.png");
+        position = new Vector2(100, 330);
+        speed = 8.0f;
+    }
+
+    public void render(SpriteBatch batch) {
+        batch.draw(texture, position.x, position.y);
+    }
+
+    public void update() {
+        if(Gdx.input.isKeyPressed(Input.Keys.UP) && position.y <= screenHeight - texture.getHeight()) {
+            position.y += speed;
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.DOWN) && position.y >= 0) {
+            position.y -= speed;
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT) && position.y <= screenWidth - texture.getWidth()) {
+            position.y += speed;
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT) && position.y >= 0) {
+            position.y -= speed;
+        }
+    }
+}
